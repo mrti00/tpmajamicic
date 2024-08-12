af4d97e9b854d1c11632fb82788740eb3c7be394
mrti00 <micicmaarko@gmail.com>
2024-08-12 15:15:39 +0200

    Prefinal deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/tpmajamicic/",
+  base: "/",
   server: {
     host: true,
     port: 3000,
