59bae9458b025dd2bf5b230f7a1db4a42b77d48d
mrti00 <micicmaarko@gmail.com>
2024-08-14 21:57:24 +0200

    Final Deployment (everything is checked,fixed,etc...)

--- src/pages/Usluge.jsx
+++ src/pages/Usluge.jsx
@@ -15,51 +15,51 @@ import TypewriterComponent from "typewriter-effect";
 import Faq from "../components/Faq";
 
 const materials = [
-  { name: "Karton", icon: <FaBoxOpen className="text-gray-800 text-3xl" /> },
+  { name: "Karton", icon: <FaBoxOpen className="text-3xl text-gray-800" /> },
   {
     name: "Plastiku",
-    icon: <MdLocalDrink className="text-gray-800 text-3xl" />,
+    icon: <MdLocalDrink className="text-3xl text-gray-800" />,
   },
   {
     name: "Papir",
-    icon: <RiFilePaperLine className="text-gray-800 text-3xl" />,
+    icon: <RiFilePaperLine className="text-3xl text-gray-800" />,
   },
   {
     name: "Staklene ambalaže",
-    icon: <PiBeerBottleBold className="text-gray-800 text-3xl" />,
+    icon: <PiBeerBottleBold className="text-3xl text-gray-800" />,
   },
   {
     name: "Plastične flaše",
-    icon: <FaPrescriptionBottle className="text-gray-800 text-3xl" />,
+    icon: <FaPrescriptionBottle className="text-3xl text-gray-800" />,
   },
   {
     name: "Streč foliju",
-    icon: <GiRibbon className="text-gray-800 text-3xl" />,
+    icon: <GiRibbon className="text-3xl text-gray-800" />,
   },
   {
     name: "Polietilensku foliju",
-    icon: <GiFilmSpool className="text-gray-800 text-3xl" />,
+    icon: <GiFilmSpool className="text-3xl text-gray-800" />,
   },
-  { name: "Najlon", icon: <FaRegCircle className="text-gray-800 text-3xl" /> },
+  { name: "Najlon", icon: <FaRegCircle className="text-3xl text-gray-800" /> },
 ];
 
 const Usluge = () => {
   return (
     <main>
       <section>
-        <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-6 text-center">
+        <h2 className="mb-6 text-center text-3xl font-bold text-gray-800 md:text-4xl">
           Usluge
         </h2>
 
-        <div className="relative border border-gray-300 p-4 rounded-lg shadow-lg mb-12">
+        <div className="relative mb-12 rounded-lg border border-gray-300 p-4 shadow-lg">
           <div className="relative">
             <img
               src={boxesofboxes}
               alt="Kartonske kutije"
-              className="w-full h-[300px] md:h-[400px] object-cover shadow-md"
+              className="h-[300px] w-full object-cover shadow-md md:h-[400px]"
             />
-            <div className="absolute inset-0 bg-gray-900 bg-opacity-70 flex items-center justify-center">
-              <h2 className="text-2xl md:text-3xl text-white text-center flex flex-col md:flex-row items-center whitespace-nowrap">
+            <div className="absolute inset-0 flex items-center justify-center bg-gray-900 bg-opacity-70">
+              <h2 className="flex flex-col items-center whitespace-nowrap text-center text-2xl text-white md:flex-row md:text-3xl">
                 <span>Prodaja kutija za </span>
                 <span className="ml-2">
                   <TypewriterComponent
@@ -76,8 +76,8 @@ const Usluge = () => {
             </div>
           </div>
           <div className="relative mt-12">
-            <div className="flex items-center mb-6">
-              <FaCartPlus className="text-gray-800 text-3xl mr-4" />
+            <div className="mb-6 flex items-center">
+              <FaCartPlus className="mr-4 text-3xl text-gray-800" />
               <h3 className="text-2xl text-gray-800">Prodaja</h3>
             </div>
             <p className="mb-4">
@@ -89,23 +89,23 @@ const Usluge = () => {
           </div>
         </div>
 
-        <div className="border border-gray-300 p-4 rounded-lg shadow-lg mb-10">
-          <div className="flex items-center mb-6">
-            <FaRecycle className="text-gray-800 text-3xl mr-4" />
+        <div className="mb-10 rounded-lg border border-gray-300 p-4 shadow-lg">
+          <div className="mb-6 flex items-center">
+            <FaRecycle className="mr-4 text-3xl text-gray-800" />
             <h3 className="text-2xl text-gray-800">Otkup</h3>
           </div>
           <p className="mb-4">
             Naša firma se bavi otkupom različitih vrsta sekundarnih materijala
             širom Srbije. Otkupljujemo:
           </p>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
             {materials.map((material) => (
               <div
                 key={material.name}
-                className="bg-white border border-gray-300 p-4 rounded-lg shadow-md flex items-center space-x-4"
+                className="flex items-center space-x-4 rounded-lg border border-gray-300 bg-white p-4 shadow-md"
               >
                 {material.icon}
-                <p className="text-gray-800 text-lg">{material.name}</p>
+                <p className="text-lg text-gray-800">{material.name}</p>
               </div>
             ))}
           </div>
@@ -124,12 +124,12 @@ const Usluge = () => {
             Takođe, obezbeđujemo preuzimanje materijala sa vaše lokacije širom
             Srbije, čime štitimo vaš prostor i olakšavamo proces reciklaže.
           </p>
-          <hr className="border-t-2 border-transparent mt-5 md:mt-10 bg-gradient-to-r from-[#F0F7EE] via-white to-[#F0F7EE] h-1" />
-          <div className="flex items-center mt-6 justify-center">
-            <FaTruck className="text-gray-800 text-3xl mr-4 mt-6" />
+          <hr className="mt-5 h-1 border-t-2 border-transparent bg-gradient-to-r from-[#F0F7EE] via-white to-[#F0F7EE] md:mt-10" />
+          <div className="mt-6 flex items-center justify-center">
+            <FaTruck className="mr-4 text-3xl text-gray-800" />
             <p>Brza i efikasna dostava širom Srbije.</p>
           </div>
-          <hr className="border-t-2 border-transparent mt-5 md:mt-10 bg-gradient-to-r from-[#F0F7EE] via-white to-[#F0F7EE] h-1" />
+          <hr className="mt-5 h-1 border-t-2 border-transparent bg-gradient-to-r from-[#F0F7EE] via-white to-[#F0F7EE] md:mt-10" />
         </div>
 
         <Faq />
